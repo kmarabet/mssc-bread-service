f9f6a94c7663720d9b7fa8fb3ccfbb5f54cc5029
kmarabet <kamilmarabet@gmail.com>
2019-12-18 20:47:58 +0300

    adding web data model. closes #1

---
+++ src/main/java/guru/springframework/msscbreadservice/web/model/BreadPagedList.java
@@ -0,0 +1,17 @@
+package guru.springframework.msscbreadservice.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class BreadPagedList extends PageImpl<BreadDto> {
+
+    public BreadPagedList(List<BreadDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BreadPagedList(List<BreadDto> content) {
+        super(content);
+    }
+}
---
+++ src/main/java/guru/springframework/msscbreadservice/web/model/BreadStypeEnum.java
@@ -0,0 +1,5 @@
+package guru.springframework.msscbreadservice.web.model;
+
+public enum BreadStypeEnum {
+    ARABIC, RUSSIAN, TURKISH, FRENCH, BRITISH
+}

ab7a2451902662e9c937dea38ab46b4d2839f5f8
kmarabet <kamilmarabet@gmail.com>
2019-12-27 20:52:11 +0300

    Adding jsoncreator example - closes #13

--- src/main/java/guru/springframework/msscbreadservice/web/model/BreadPagedList.java
+++ src/main/java/guru/springframework/msscbreadservice/web/model/BreadPagedList.java
@@ -1,11 +1,34 @@
 package guru.springframework.msscbreadservice.web.model;
 
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class BreadPagedList extends PageImpl<BreadDto> {
+public class BreadPagedList extends PageImpl<BreadDto> implements Serializable {
+
+    static final long serialVersionUID = 1114715135625836949L;
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BreadPagedList(@JsonProperty("content") List<BreadDto> content,
+                         @JsonProperty("number") int number,
+                         @JsonProperty("size") int size,
+                         @JsonProperty("totalElements") Long totalElements,
+                         @JsonProperty("pageable") JsonNode pageable,
+                         @JsonProperty("last") boolean last,
+                         @JsonProperty("totalPages") int totalPages,
+                         @JsonProperty("sort") JsonNode sort,
+                         @JsonProperty("first") boolean first,
+                         @JsonProperty("numberOfElements") int numberOfElements) {
+
+        super(content, PageRequest.of(number, size), totalElements);
+    }
 
     public BreadPagedList(List<BreadDto> content, Pageable pageable, long total) {
         super(content, pageable, total);

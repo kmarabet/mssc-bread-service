51674384f2cd396b8d0519f5efaacdeff86b9869
kmarabet <kamilmarabet@gmail.com>
2020-01-15 16:27:54 +0300

    Configure Hikari Connection Pool for MySQL, closes #20

---
+++ src/main/java/guru/springframework/msscbreadservice/config/JmsConfig.java
@@ -0,0 +1,22 @@
+package guru.springframework.msscbreadservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    public static final String MY_QUEUE = "my-hello-world";
+    public static final String MY_SEND_RCV_QUEUE = "reply-back-to-me";
+
+    @Bean
+    MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
